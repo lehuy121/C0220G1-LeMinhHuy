014b6fe401294951b16898edb7e8168ea3f9f347
C0220G1-LeMinhHuy <lehuy121@gmail.com>
2020-02-29 14:11:58 +0700

    refactor code Task1+2

--- module1/CaseStudy_FuramaResort/myScripts.js
+++ module1/CaseStudy_FuramaResort/myScripts.js
@@ -1,77 +1,84 @@
-function confirmAction(){
+function confirmAction() {
     let confirmMessage = confirm("Are you sure ?");
-    if(confirmMessage){
-        displayInfor();
+    if (confirmMessage) {
+        displayInformation();
     }
 }
-function displayInfor() {
-    let labelCustomerName = "Customer Name: ";
-    let labelTotalPayment = "TotalPayment: ";
-    let labelIdNumber = "ID Number: ";
-    let labelBirth = "Day Of Birth: ";
-    let labelEmail = "Email: ";
-    let labelAddr = "Address: ";
-    let labelCustomerType = "Customer Type: ";
-    let labelDiscount = "Discount: ";
-    let labelQuantity = "Quantity Includes: ";
-    let labelRentDays = "Rent Days: ";
-    let labelServiceType = "Service Type: ";
-    let labelPercent = "%";
+function displayInformation() {
+    let initData = {
+        customerName: getValueById("customerName"),
+        idNumber: getValueById("idNumber"),
+        email: getValueById("email"),
+        dayOfBirth: getValueById("dayOfBirth"),
+        address: getValueById("addr"),
+        customerType: getTextOptionSelected(getElementById("customerType")),
+        discount: getValueById("discount"),
+        quantity: getValueById("quantity"),
+        rentDays: getValueById("rentDays"),
+        serviceType: getTextOptionSelected(getElementById("serviceType")),
+        valueServiceType: getValueById("serviceType"),
+        roomType: getValueById("roomType")
+    };
+    let label = {
+        customerName: "CustomerName: ",
+        totalPayment: "TotalPayment: ",
+        idNumber: "ID Number: ",
+        labelBirth: "Day Of Birth: ",
+        email: "Email: ",
+        address: "Address: ",
+        customerType: "Customer Type: ",
+        discount: "Discount: ",
+        quantity: "Quantity Includes: ",
+        rentDays: "Rent Days: ",
+        serviceType: "Service Type: ",
+        roomType: "Room Type: ",
+        percent: " %",
+        usd: " $"
+    };
+    pushTextValueById("showName", label.customerName + initData.customerName);
+    pushTextValueById("showIDNumber", label.idNumber + initData.idNumber);
+    pushTextValueById("showBirth", label.labelBirth + initData.dayOfBirth);
+    pushTextValueById("showEmail", label.email + initData.email);
+    pushTextValueById("showAddr", label.address + initData.address);
+    pushTextValueById("showCustomerType", label.customerType + initData.customerType);
+    pushTextValueById("showDiscount", label.discount + initData.discount + label.percent);
+    pushTextValueById("showQuantity", label.quantity + initData.quantity);
+    pushTextValueById("showRentDays", label.rentDays + initData.rentDays);
+    pushTextValueById("showServiceType", label.serviceType + initData.serviceType);
+    pushTextValueById("showRoomType", label.roomType + initData.roomType);
 
-    let customerName = getElementByID("customerName").value;
-    pushValueToHtmlById("showName").innerText = labelCustomerName + customerName;
-
-    let idNumber = getElementByID("idNumber").value;
-    pushValueToHtmlById("showIDNumber").innerText = labelIdNumber + idNumber;
-
-    let birth = getElementByID("dayOfBirth").value;
-    pushValueToHtmlById("showBirth").innerText = labelBirth + birth;
-
-    let email = getElementByID("email").value;
-    pushValueToHtmlById("showEmail").innerText = labelEmail + email;
-
-    let address = getElementByID("addr").value;
-    pushValueToHtmlById("showAddr").innerText = labelAddr + address;
-
-    let customerType = getElementByID("customerType");
-    let txtCustomerType = getTextOptionSelected(customerType);
-    pushValueToHtmlById("showCustomerType").innerText = labelCustomerType + txtCustomerType;
-
-    let discount = getElementByID("discount").value;
-    pushValueToHtmlById("showDiscount").innerText = labelDiscount + discount + labelPercent;
-
-    let quantity = getElementByID("quantity").value;
-    pushValueToHtmlById("showQuantity").innerText = labelQuantity + quantity;
-
-    let rentDays = getElementByID("rentDays").value;
-    pushValueToHtmlById("showRentDays").innerText = labelRentDays + rentDays;
-
-    let serviceType = getElementByID("serviceType");
-    let displayText = getTextOptionSelected(serviceType);
-    pushValueToHtmlById("showServiceType").innerText = labelServiceType + displayText;
-    let getServiceType = serviceType.value;
-
-    let roomType = getElementByID("roomType").value;
-    pushValueToHtmlById("showRoomType").innerText = "Room Type: " + roomType;
-
-    let result = paymentCalculator(getServiceType,rentDays,discount);
-    pushValueToHtmlById("totalPay").innerText = labelTotalPayment + result.toString();
+    let result = paymentCalculator(initData.valueServiceType, initData.rentDays, initData.discount);
+    pushTextValueById("totalPay", label.totalPayment + result + label.usd);
 }
-function paymentCalculator(serviceType, rentDays, discount){
+
+function paymentCalculator(serviceType, rentDays, discount) {
     let payment = serviceType * rentDays;
     let totalPayment = 0;
-    return totalPayment = payment *(1 - discount/100);
+    return totalPayment = payment * (1 - discount / 100);
 }
+
 function insertToDiscount() {
-    let getCustomerType = getElementByID("customerType");
-    pushValueToHtmlById("discount").value = getCustomerType.value;
+    let getCustomerType = getValueById("customerType");
+    // getValueById("discount") = getCustomerType;
+    pushValueById("discount", getCustomerType);
 }
-function getElementByID(id){
+
+function getElementById(id) {
     return document.getElementById(id);
 }
-function pushValueToHtmlById(id){
-    return document.getElementById(id);
+
+function getValueById(id) {
+    return document.getElementById(id).value;
 }
-function getTextOptionSelected(element){
-   return element.options[element.selectedIndex].text;
+
+function pushValueById(id, elementValue) {
+    return document.getElementById(id).value = elementValue;
+}
+
+function pushTextValueById(id, elementValue) {
+    return document.getElementById(id).innerText = elementValue;
+}
+
+function getTextOptionSelected(element) {
+    return element.options[element.selectedIndex].text;
 }
