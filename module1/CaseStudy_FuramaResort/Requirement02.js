1346696476e7999e4f82d3a2cc625429a48d4ef5
C0220G1-LeMinhHuy <lehuy121@gmail.com>
2020-03-03 21:17:10 +0700

    Requirement02-Task02

--- module1/CaseStudy_FuramaResort/Requirement02.js
+++ module1/CaseStudy_FuramaResort/Requirement02.js
@@ -1,4 +1,4 @@
-let initData = {
+let customer = {
     customerName: "Le Minh Huy",
     idNumber: 123456789,
     birthDay: "02/03/2020",
@@ -12,43 +12,64 @@ let initData = {
     roomType: "Vip"
 };
 let downLine = "\n";
-let showCustomerName = "Customer Name: " + initData.customerName + downLine;
-let showIdNumber = "ID Number: " + initData.idNumber + downLine;
-let showBirth = "Birth: " + initData.birthDay + downLine;
-let showEmail = "Email: " + initData.email + downLine;
-let showAddress = "Address: " + initData.address + downLine;
-let showCustomerType = "Customer Type: " + initData.customerType + downLine;
-let showDiscount = "Discount: " + initData.discount + downLine;
-let showQuantity = "Quantity Includes: " + initData.quantityIncludes + downLine;
-let showRendDays = "Rend Days: " + initData.rendDays + downLine;
-let showServiceType = "Service Type: " + initData.serviceType + downLine;
-let showRoomType = "Room Type: " + initData.roomType + downLine;
+let showCustomerName = "Customer Name: " + customer.customerName + downLine;
+let showIdNumber = "ID Number: " + customer.idNumber + downLine;
+let showBirth = "Birth: " + customer.birthDay + downLine;
+let showEmail = "Email: " + customer.email + downLine;
+let showAddress = "Address: " + customer.address + downLine;
+let showCustomerType = "Customer Type: " + customer.customerType + downLine;
+let showDiscount = "Discount: " + customer.discount + downLine;
+let showQuantity = "Quantity Includes: " + customer.quantityIncludes + downLine;
+let showRendDays = "Rend Days: " + customer.rendDays + downLine;
+let showServiceType = "Service Type: " + customer.serviceType + downLine;
+let showRoomType = "Room Type: " + customer.roomType + downLine;
 
 let menu = prompt("Enter 1 : Show Information Customer" + downLine + "Enter 2 : Edit Information Customer");
 let editName = "Enter 1: Edit Name" + downLine;
 let editBirth = "Enter 2: Birth" + downLine;
 let editAdds = "Enter 3: Address" + downLine;
+let editRentDays = "Enter 4: RentDays" + downLine;
 if (menu === "1") {
     alert(showInformation());
 } else if (menu === "2") {
-    let pickEdit = prompt(editName + editBirth + editAdds);
-    if (pickEdit === "1") {
+    let editMenu = prompt(editName + editBirth + editAdds + editRentDays);
+    if (editMenu === "1") {
         let newName = prompt("Enter New Name: ");
-        alert(updateInformation());
-    }else if(pickEdit ==="2"){
-       let newBirth = prompt("Enter New Birth: ");
-    }else if(pickEdit ==="3"){
+        alert(updateCustomerName(newName));
+    } else if (editMenu === "2") {
+        let newBirth = prompt("Enter New Birth: ");
+        alert(updateBirth(newBirth));
+    } else if (editMenu === "3") {
         let newAddress = prompt("Enter New Address: ");
-    }else {
+        alert(updateAddress(newAddress));
+    } else if (editMenu === "4") {
+        let newRentDays = prompt("Enter New Address: ");
+        alert(updateRentDays(newRentDays));
+    } else {
         alert("Failed");
     }
 }
-function showInformation(){
+
+function showInformation() {
     return showCustomerName + showIdNumber + showBirth + showEmail + showAddress + showCustomerType + showDiscount + showQuantity + showRendDays + showServiceType + showRoomType;
-}
-function calculatorPayment(){
 
 }
-function updateInformation(){
-    return showInformation();
+
+function calculatorPayment() {
+
+}
+
+function updateCustomerName(newName) {
+    return "CustomerName: " + newName + downLine + showIdNumber + showBirth + showEmail + showAddress + showCustomerType + showDiscount + showQuantity + showRendDays + showServiceType + showRoomType;
+}
+
+function updateBirth(newBirth) {
+    return showCustomerName + showIdNumber + "Birth: " + newBirth + downLine + showEmail + showAddress + showCustomerType + showDiscount + showQuantity + showRendDays + showServiceType + showRoomType;
+}
+
+function updateAddress(newAddress) {
+    return showCustomerName + showIdNumber + showBirth + showEmail + "Address: " + newAddress + downLine + showCustomerType + showDiscount + showQuantity + showRendDays + showServiceType + showRoomType;
+}
+function updateRentDays(newRentDays){
+    return showCustomerName + showIdNumber + showBirth + showEmail + showAddress + showCustomerType + showDiscount + showQuantity + "RentDays: " + newRentDays + downLine + showServiceType + showRoomType;
 }
