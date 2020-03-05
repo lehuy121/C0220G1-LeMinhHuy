5b2a8fa1b3ee4cfa6639064a36c4880e69b69b24
C0220G1-LeMinhHuy <lehuy121@gmail.com>
2020-03-05 23:53:32 +0700

    Requirement02 Task03

--- module1/CaseStudy_FuramaResort/Requirement02.js
+++ module1/CaseStudy_FuramaResort/Requirement02.js
@@ -5,12 +5,35 @@ let customer = {
     email: "lehuy121@mail.com",
     address: "Da Nang",
     customerType: "Diamond",
-    discount: 50,
     quantityIncludes: 2,
-    rendDays: 2,
+    rendDays: 7,
     serviceType: "Villa",
-    roomType: "Vip"
+    roomType: "Vip",
 };
+let discountByAddress = {
+    daNang: 20,
+    hue: 10,
+    quangNgai: 5
+};
+let discountByRentDays = {
+    above7: 30,
+    range5To7: 20,
+    range2To4: 10
+};
+let discountByCustomerType = {
+    diamond: 15,
+    platinum: 10,
+    gold: 5,
+    silver: 2,
+    member: 1
+};
+let priceServiceType = {
+    villa: 500,
+    house: 300,
+    room: 100
+};
+let discountPayment = discountAmountCalculate(discountByAddress.daNang, discountByRentDays.above7, discountByCustomerType.diamond);
+let payment = paymentCalculate(priceServiceType.villa, customer.rendDays, discountPayment);
 let downLine = "\n";
 let showCustomerName = "Customer Name: " + customer.customerName + downLine;
 let showIdNumber = "ID Number: " + customer.idNumber + downLine;
@@ -18,17 +41,20 @@ let showBirth = "Birth: " + customer.birthDay + downLine;
 let showEmail = "Email: " + customer.email + downLine;
 let showAddress = "Address: " + customer.address + downLine;
 let showCustomerType = "Customer Type: " + customer.customerType + downLine;
-let showDiscount = "Discount: " + customer.discount + downLine;
+let showDiscount = "Discount: " + discountPayment + downLine;
 let showQuantity = "Quantity Includes: " + customer.quantityIncludes + downLine;
 let showRendDays = "Rend Days: " + customer.rendDays + downLine;
 let showServiceType = "Service Type: " + customer.serviceType + downLine;
 let showRoomType = "Room Type: " + customer.roomType + downLine;
+let showPayment = "Payment : " + payment + downLine;
 
 let menu = prompt("Enter 1 : Show Information Customer" + downLine + "Enter 2 : Edit Information Customer");
 let editName = "Enter 1: Edit Name" + downLine;
 let editBirth = "Enter 2: Birth" + downLine;
 let editAdds = "Enter 3: Address" + downLine;
 let editRentDays = "Enter 4: RentDays" + downLine;
+
+
 if (menu === "1") {
     alert(showInformation());
 } else if (menu === "2") {
@@ -43,33 +69,52 @@ if (menu === "1") {
         let newAddress = prompt("Enter New Address: ");
         alert(updateAddress(newAddress));
     } else if (editMenu === "4") {
-        let newRentDays = prompt("Enter New Address: ");
-        alert(updateRentDays(newRentDays));
+        let newRentDays = parseInt(prompt("Enter Rent Days: "));
+        if (newRentDays < 5) {
+            updateRentDays(newRentDays,discountByRentDays.range2To4);
+        } else if (newRentDays < 8) {
+            updateRentDays(newRentDays, discountByRentDays.range5To7);
+        } else {
+            updateRentDays(newRentDays, discountByRentDays.above7);
+        }
     } else {
         alert("Failed");
     }
 }
 
 function showInformation() {
-    return showCustomerName + showIdNumber + showBirth + showEmail + showAddress + showCustomerType + showDiscount + showQuantity + showRendDays + showServiceType + showRoomType;
-
-}
-
-function calculatorPayment() {
-
+    return showCustomerName + showIdNumber + showBirth + showEmail + showAddress + showCustomerType + showDiscount + showQuantity + showRendDays + showServiceType + showRoomType + showPayment;
 }
 
 function updateCustomerName(newName) {
-    return "CustomerName: " + newName + downLine + showIdNumber + showBirth + showEmail + showAddress + showCustomerType + showDiscount + showQuantity + showRendDays + showServiceType + showRoomType;
+    return "CustomerName: " + newName + downLine + showIdNumber + showBirth + showEmail + showAddress + showCustomerType + showDiscount + showQuantity + showRendDays + showServiceType + showRoomType + showPayment;
 }
 
 function updateBirth(newBirth) {
-    return showCustomerName + showIdNumber + "Birth: " + newBirth + downLine + showEmail + showAddress + showCustomerType + showDiscount + showQuantity + showRendDays + showServiceType + showRoomType;
+    return showCustomerName + showIdNumber + "Birth: " + newBirth + downLine + showEmail + showAddress + showCustomerType + showDiscount + showQuantity + showRendDays + showServiceType + showRoomType + showPayment;
 }
 
 function updateAddress(newAddress) {
-    return showCustomerName + showIdNumber + showBirth + showEmail + "Address: " + newAddress + downLine + showCustomerType + showDiscount + showQuantity + showRendDays + showServiceType + showRoomType;
+    return showCustomerName + showIdNumber + showBirth + showEmail + "Address: " + newAddress + downLine + showCustomerType + showDiscount + showQuantity + showRendDays + showServiceType + showRoomType + showPayment;
 }
-function updateRentDays(newRentDays){
-    return showCustomerName + showIdNumber + showBirth + showEmail + showAddress + showCustomerType + showDiscount + showQuantity + "RentDays: " + newRentDays + downLine + showServiceType + showRoomType;
+function paymentCalculate(priceServiceType, rentDays, discountAmount) {
+//Số tiền phải trả  = giá loại dịch vụ * số ngày - giảm giá
+    return priceServiceType * rentDays - discountAmount;
 }
+function discountAmountCalculate(discountByAddress, discountByRentDays, discountByCustomerType) {
+// Số tiền giảm giá = giảm giá theo địa chỉ + giảm giá theo thời gian lưu trú + giảm giá theo loại Customer
+    return discountByAddress + discountByRentDays + discountByCustomerType;
+}
+function updateRentDays(newRentDays, discountRange) {
+    let newDiscountAmountRange = discountAmountCalculate(discountByAddress.daNang, discountRange, discountByCustomerType.diamond);
+    let newPaymentByRentDays = paymentCalculate(priceServiceType.villa, newRentDays, newDiscountAmountRange);
+    return alert(showCustomerName + showIdNumber + showBirth + showEmail + showAddress + showCustomerType + "New Discount: " + newDiscountAmountRange + downLine + showQuantity + "New Rent Days: " + newRentDays + downLine + showServiceType + showRoomType + "New Payment: " + newPaymentByRentDays);
+}
+
+function updateRentDaysRangeAbove7(newRentDays) {
+
+}
+
+
+
+
